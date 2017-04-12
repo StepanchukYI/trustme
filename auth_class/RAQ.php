7420873feb24dde59f280baa3cadf3ec70e93569
Stepanchuk Yevgeniy <b0dunj0@mail.ru>
2017-04-13 00:45:24 +0300

    1

---
+++ auth_class/RAQ.php
@@ -0,0 +1,125 @@
+<?php
+require "../class/sqldb_connection.php";
+/*
+ * Файл для модуля авторизации на сервере
+ * функции и методы для авторизаии(изменения данных в БД)
+ */
+
+
+/*
+ * Функция для обновления статуса ЗАРЕГЕСТРИРОВАННОГО ПОЛЬЗОВАТЕЛЯ на онлайн
+ */
+function Auth($mailornumber, $password)
+{
+    $errorArr = array();    //создание массива ошибок.
+
+    if ($mailornumber == "") array_push($errorArr, "Failed email or phone number");  // проверка на пустые поля.
+    if ($password == "") array_push($errorArr, "Failed password");  //
+
+    $tmp_db_row = sqldb_connection::Auth_Select($mailornumber);   // достаем строку из БД
+
+    if (count($tmp_db_row) == 0) {
+        array_push($errorArr, "Failed email or phone number");
+    } else {
+        if ($password != $tmp_db_row[0]['password']) array_push($errorArr, "Failed password");
+    }
+    if (count($errorArr) == 0) {
+        sqldb_connection::Update_online_status($tmp_db_row[0]['user_id'], "true");   // обновляем статус на онлайн
+        return "User online";
+    } else {
+        return $errorArr[0];
+    }
+}
+
+
+/*
+ * Функция для первичной регистрации пользователя.
+ */
+function Registration_min($name, $email, $phone, $password1, $password2)
+{
+
+    $errorArr = array();//создание массива ошибок.
+
+    //Валидация мыла
+    if ((strlen($email) <= 6) && (preg_match("~^([a-z0-9_\-\.])+@([a-z0-9_\-\.])+\.([a-z0-9])+$~i", $email) != true)) {
+        array_push($errorArr, "Incorrect email");
+    }
+    //валидация пароля
+    if ($password1 == "" && strlen($password1) <= 6 && strlen($password1) >= 32) {
+        array_push($errorArr, "Incorrect password");
+    }
+    //сравнение паролей пароля
+    if ($password1 != $password2) {
+        array_push($errorArr, "Passwords are different");
+    }
+
+    $tmp_db_row = sqldb_connection::Registration($phone, $email);
+
+    if (count($tmp_db_row) != 0) {
+        if ($tmp_db_row[0]['phone'] == $phone) array_push($errorArr, "Login already using");
+        if ($tmp_db_row[0]['email'] == $email) array_push($errorArr, "Email already using");
+    }
+
+
+    if (count($errorArr) == 0) {
+        sqldb_connection::Registration_min($name, $phone, $password1, $email, Temp_code());
+        return "User created";
+    } else {
+        return json_encode($errorArr);
+    }
+}
+
+
+/*
+ * Функция для получения временного пароля для продолжения регистрации
+ */
+function Temp_code()
+{
+    $tempcode = "";
+    for ($i = 0; $i <= 6; $i++) {
+        $tempcode .= rand(0, 9);
+    }
+    return $tempcode;
+}
+
+
+/*
+ * Функция для полной регистрации пользователя
+ */
+function Registration_full($id,$email_2, $surname, $birth_day, $birth_month, $birth_year, $sex, $country, $city)
+{
+    $errorArr = array();//создание массива ошибок.
+
+    if ((strlen($email_2) <= 6) && (preg_match("~^([a-z0-9_\-\.])+@([a-z0-9_\-\.])+\.([a-z0-9])+$~i", $email_2) != true)) {
+        array_push($errorArr, "Incorrect email");
+    }
+    if ($birth_day == "" && strlen($birth_day) < 1 && strlen($birth_day) > 31) {
+        array_push($errorArr, "Incorrect birthday");
+    }
+    if ($birth_month == "" && strlen($birth_month) < 1 && strlen($birth_month) > 12) {
+        array_push($errorArr, "Incorrect birthday month");
+    }
+    if ($birth_year == "" && strlen($birth_year) < 1930 && strlen($birth_year) > 2017) {
+        array_push($errorArr, "Incorrect birthday year");
+    }
+
+    if (count($errorArr) == 0) {
+        sqldb_connection::Registration_full($id,$email_2, $surname, $birth_day, $birth_month, $birth_year, $sex, $country, $city);
+        return "User created";
+    } else {
+        return json_encode($errorArr);
+    }
+
+
+
+
+}
+
+/*
+ * Функция для изменения статуса пользователя на офлайн
+ */
+function Quit($id)
+{
+    sqldb_connection::Update_online_status($id, "false");   // обновляем статус на офлайн
+    return "User ofline";
+}
--- class/sqldb_connection.php
+++ class/sqldb_connection.php
@@ -4,12 +4,14 @@
  * Класс для всех запросов с сервера в SQL базу.
  * (добавление, удаление, изменение)
  */
+
 class sqldb_connection
 {
     /*
      * ИНКАПСУЛИРОВАННАЯ ФУНКЦИЯ, ДЛЯ КОДКЛЮЧЕНИЮ В БАЗЕ(ИНКАПСУЛИРОВАННАЯ!!)
      */
-    private function DB_connect(){
+    private function DB_connect()
+    {
         $dsn = 'mysql:dbname=Team_4;host=127.0.0.1';
         $user = 'root';
         $password = '';
@@ -21,14 +23,56 @@ class sqldb_connection
             return 'Connection failed: ' . $e->getMessage();
         }
     }
+
     /*
-     *  Пример инкапсуляции и запроса в SQL через использование PDO
-     * это только пример для выобки. все запросы в БД посылаются по похожему принципу.
+     * Выбока из баззы значения для проверки наличия пользователя в БД
      */
-    public function Auth_Select($login){
+    public static function Auth_Select($login)
+    {
         $dbh = sqldb_connection::DB_connect();
-        $sth = $dbh->prepare("SELECT login, password,xo_online FROM clients WHERE login= :login ");
-        $sth->execute(array( ':login' => $login ));
+        $sth = $dbh->prepare("SELECT user_id, login, password,xo_online FROM clients WHERE email= :login OR phone= :login");
+        $sth->execute(array(':login' => $login));
         return $sth->fetchAll();
     }
+
+
+    /*
+     * Функция для обновления статуса ЗАРЕГЕСТРИРОВАННОГО ПОЛЬЗОВАТЕЛЯ на онлайл или офлайн
+     */
+    public static function Update_online_status($id, $value)
+    {
+        $dbh = sqldb_connection::DB_connect();
+        $sth = $dbh->prepare("UPDATE user SET online_status =:online_status  WHERE user_ID =:id");
+        $sth->execute(array(':online_status' => $value, ':id' => $id));
+    }
+
+    /*
+     * Функция для выборки и сравнения данных первичной регистрации пользователя.
+     */
+    public static function Registration($phone, $email)
+    {
+        $dbh = sqldb_connection::DB_connect();
+        $sth = $dbh->prepare("SELECT login,email FROM clients WHERE phone=:phone OR email =:email ");
+        $sth->execute(array(':phone' => $phone, ':email' => $email));
+        return $sth->fetchAll();
+    }
+
+    public static function Registration_min($name, $phone, $password, $email, $code)
+    {
+        $dbh = sqldb_connection::DB_connect();
+        $sth = $dbh->prepare("INSERT INTO user(name,password,email,phone,temp_code ) 
+                          VALUES(:login, :password, :email, :phone, :code)");
+        $sth->execute(array(':name' => $name, ':password' => $password, ':email' => $email, ':phone' => $phone, ':code' => $code));
+    }
+
+    public static function Registration_full($email, $email_2, $surname, $birth_day, $birth_month, $birth_year, $sex, $country, $city)
+    {
+        $dbh = sqldb_connection::DB_connect();
+        $sth = $dbh->prepare("UPDATE user SET email_2=:email_2, surname =: surname, birth_day = :birth_day, birth_month =:
+        :birth_month, birth_year =:birth_year, sex = :sex, country = :country, city = :city WHERE email=:email");
+        $sth->execute(array(':email_2' => $email_2, ':surname' => $surname, ':birth_day' => $birth_day,
+            ':birth_month' => $birth_month, ':birth_year' => $birth_year, ':sex' => $sex, ':country' => $country,
+            ':city' => $city, ':email' => $email,));
+    }
 }
+
