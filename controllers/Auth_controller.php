ed801a9eff2c27fad17b31800e1f423ebc4d9fe2
Stepanchuk Yevgeniy <b0dunj0@mail.ru>
2017-04-16 21:41:39 +0300

    post на авторизацию пока

--- controllers/Auth_controller.php
+++ controllers/Auth_controller.php
@@ -1,12 +1,12 @@
 <?php
 require "../auth_class/RAQ.php";
 
-$command = $_GET['command'];
+$command = $_POST['command'];
 
 switch ($command) {
     case "auth": //http://37.57.92.40/trustme/controllers/auth_controller.php?command=auth&login=bodunjo855@gmail.com&password=rootttt
-        $login = $_GET['login'];
-        $password = $_GET['password'];
+        $login = $_POST['login'];
+        $password = $_POST['password'];
 
         if ($login != "" && $password != "") {
             echo Auth($login, $password);
@@ -15,10 +15,10 @@ switch ($command) {
         }
         break;
     case "reg_min": //http://37.57.92.40/trustme/controllers/auth_controller.php?command=reg_min&email=&phone=809503856636616&password1=rootttt&password2=rootttt
-        $email = $_GET['email'];
-        $phone = $_GET['phone'];
-        $password1 = $_GET['password1'];
-        $password2 = $_GET['password2'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $password1 = $_POST['password1'];
+        $password2 = $_POST['password2'];
 
         if ($email != "" && $phone != "" && $password1 != "" && $password1 != "") {
             echo Registration_min($email, $phone, $password1, $password2);
@@ -27,16 +27,16 @@ switch ($command) {
         }
         break;
     case "reg_full": //http://37.57.92.40/trustme/controllers/auth_controller.php?command=reg_full&id=1&email_2=fsdfsd@mads.ru&name=dasdasd&surname=fsdfsdfsdf&birth_day=14&birth_month=3&birth_year=1996&sex=1&country=Ucraine&city=Dnepro
-        $id = $_GET['id'];
-        $email_2 = $_GET['email_2'];
-        $name = $_GET['name'];
-        $surname = $_GET['surname'];
-        $birth_day = $_GET['birth_day'];
-        $birth_month = $_GET['birth_month'];
-        $birth_year = $_GET['birth_year'];
-        $sex = $_GET['sex'];
-        $country = $_GET['country'];
-        $city = $_GET['city'];
+        $id = $_POST['id'];
+        $email_2 = $_POST['email_2'];
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $birth_day = $_POST['birth_day'];
+        $birth_month = $_POST['birth_month'];
+        $birth_year = $_POST['birth_year'];
+        $sex = $_POST['sex'];
+        $country = $_POST['country'];
+        $city = $_POST['city'];
 
         if ($id != "" && $email_2 != "" && $name != "" && $surname != "" && $birth_day != ""
             && $birth_month != "" && $birth_year != "" && $sex != "" && $country != "" && $city != "") {
@@ -46,7 +46,7 @@ switch ($command) {
         }
         break;
     case "quit": //http://37.57.92.40/trustme/controllers/auth_controller.php?command=quit&id=1
-        $id = $_GET['id'];
+        $id = $_POST['id'];
         if($id != ""){
             echo Quit($id);
         }else{
